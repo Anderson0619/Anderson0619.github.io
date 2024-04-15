03ce70166416cf936d18ae7ae04a80c1e2c5a325
Anderson0619 <ameramurillo869@gmail.com>
2024-04-15 09:30:32 -0500

    cambios

--- comentario.js
+++ comentario.js
@@ -1,14 +1,14 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
+document.getElementById('contactForm-general').addEventListener('submit', function(event) {
   event.preventDefault(); // Prevent default form submission
   
   // Get form values
-  var name = document.getElementById('name').value;
-  var email = document.getElementById('email').value;
-  var message = document.getElementById('message').value;
+  var name = document.getElementById('name-general').value;
+  var email = document.getElementById('email-general').value;
+  var message = document.getElementById('message-general').value;
 
   // Construct the mailto URL
-  var mailtoLink = 'mailto:sistemas@fishcorpsa.com.ec' +
-                   '?subject=' + encodeURIComponent('Mensaje de contacto desde el sitio web') +
+  var mailtoLink = 'mailto:trabajosocial@fishcorpsa.com.ec' +
+                   '?subject=' + encodeURIComponent('Mensaje de contacto desde el sitio web Fishcorp') +
                    '&body=' + encodeURIComponent('Nombre: ' + name + '\nCorreo Electrónico: ' + email + '\nMensaje: ' + message);
 
   // Open the mail client with the pre-filled values
